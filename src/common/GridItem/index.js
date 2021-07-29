f59eaa3a731df89e83712b6a43bd456e2e40711a
SojeongLee <kather0220@ewhain.net>
2021-07-29 21:48:50 +0900

    (grid-item) feat : change image onclick heart icon

--- src/common/GridItem/index.js
+++ src/common/GridItem/index.js
@@ -1,13 +1,22 @@
 import * as S from './styles';
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 function GridItem(props) {
   const history = useHistory();
+  const [clicked, setClicked] = useState(false);
   const event_id = 'whopper-event'; // 임시값. 서버에서 넘겨 받을 예정
   const handleClick = (e) => {
     e.preventDefault();
     history.push(`/event-detail/${event_id}`);
+  };
+  const handleHeartClick = (e) => {
+    e.preventDefault();
+    //setClicked(!clicked);
+
+    setClicked(!clicked);
+    if (!clicked)
+      alert('좋아요가 등록되었습니다. 마이벤티 페이지에서 확인해주세요.');
   };
   return (
     <>
@@ -19,7 +28,10 @@ function GridItem(props) {
           <S.GridImage src={'/img/whopper-event.png'} onClick={handleClick} />
           <S.FirstRow>
             <S.EventName>6월 와퍼 할인 이벤트</S.EventName>
-            <S.HeartIcon src={'/img/heart.png'} />
+            <S.HeartIcon
+              onClick={handleHeartClick}
+              src={clicked ? '/img/clicked-heart.png' : '/img/heart.png'}
+            />
           </S.FirstRow>
           <S.SecondRow>버거킹</S.SecondRow>
           <S.ThirdRow>D-5{'     '}조회 130회</S.ThirdRow>
