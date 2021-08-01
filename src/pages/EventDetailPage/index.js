332912de051c84e68af054ff3fe32cbb16e06924
SojeongLee <kather0220@ewhain.net>
2021-08-01 20:40:21 +0900

    (event-detail) style : change icon img onclick heart

--- src/pages/EventDetailPage/index.js
+++ src/pages/EventDetailPage/index.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../common/Header/index';
 //import Button from '../../common/Button/index';
 import * as S from './styles';
 
 function EventDetailPage() {
+  const [clicked, setClicked] = useState(false);
+  const handleHeartClick = (e) => {
+    e.preventDefault();
+    //setClicked(!clicked);
+
+    setClicked(!clicked);
+    if (!clicked) {
+      setTimeout(function () {
+        alert('좋아요가 등록되었습니다. 마이벤티 페이지에서 확인해주세요.');
+      }, 100);
+    }
+    //alert('좋아요가 등록되었습니다. 마이벤티 페이지에서 확인해주세요.');
+  };
   return (
     <>
       <Header></Header>
@@ -14,7 +27,10 @@ function EventDetailPage() {
             <br></br>2020.09.09
           </S.BrandNameAndDate>
           <S.EventName>6월 와퍼 할인 이벤트</S.EventName>
-          <S.HeartIcon src={'/img/heart.png'}></S.HeartIcon>
+          <S.HeartIcon
+            onClick={handleHeartClick}
+            src={clicked ? '/img/clicked-heart.png' : '/img/heart.png'}
+          ></S.HeartIcon>
         </S.EvenInfoWrapper>
         <S.EventImage src={'/img/event-detail-example.png'}></S.EventImage>
         <S.EventText>
