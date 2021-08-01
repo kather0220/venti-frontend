87619467e7d9f39af3c53d556c1e9d006abf6f28
SojeongLee <kather0220@ewhain.net>
2021-08-02 04:26:23 +0900

    (withdrawal) feat : save radio button input and deal with withdrawal button

--- src/pages/WithdrawalPage/index.js
+++ src/pages/WithdrawalPage/index.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import * as S from './styles';
 import Header from '../../common/Header/index';
 import Footer from '../../common/Footer/index';
 
 function WithdrawalPage() {
+  const [clicked, setClicked] = useState('');
+  const history = useHistory();
+  const handleRadioClick = (e) => {
+    switch (e.target.value) {
+      case '0':
+        setClicked('0');
+        break;
+      case '1':
+        setClicked('1');
+        break;
+      case '2':
+        setClicked('2');
+        break;
+      case '3':
+        setClicked('3');
+        break;
+      default:
+        break;
+    }
+  };
+  const handleClick = () => {
+    if (clicked === '') alert('탈퇴 이유를 알려주세요!');
+    else {
+      alert('탈퇴 완료되었습니다. 벤티를 이용해주셔서 감사드립니다.');
+      history.push('/');
+    }
+  };
   return (
     <>
       <Header></Header>
@@ -18,8 +46,8 @@ function WithdrawalPage() {
             <S.RadioButton
               type="radio"
               value="0"
-              //onClick={handleRadioClick}
-              //checked={isMale}
+              onClick={handleRadioClick}
+              checked={clicked === '0'}
             ></S.RadioButton>
             이벤트를 보고싶은 브랜드가 없어서
           </S.ButtonLabel>
@@ -27,8 +55,8 @@ function WithdrawalPage() {
             <S.RadioButton
               type="radio"
               value="1"
-              //onClick={handleRadioClick}
-              //checked={isMale}
+              onClick={handleRadioClick}
+              checked={clicked === '1'}
             ></S.RadioButton>
             원하는 이벤트가 없어서
           </S.ButtonLabel>
@@ -36,8 +64,8 @@ function WithdrawalPage() {
             <S.RadioButton
               type="radio"
               value="2"
-              //onClick={handleRadioClick}
-              //checked={isMale}
+              onClick={handleRadioClick}
+              checked={clicked === '2'}
             ></S.RadioButton>
             VENTI 서비스를 사용해도 이벤트를 놓쳐서
           </S.ButtonLabel>
@@ -45,12 +73,12 @@ function WithdrawalPage() {
             <S.RadioButton
               type="radio"
               value="3"
-              //onClick={handleRadioClick}
-              //checked={isMale}
+              onClick={handleRadioClick}
+              checked={clicked === '3'}
             ></S.RadioButton>
             기타
           </S.ButtonLabel>
-          <S.Button>회원 탈퇴하기</S.Button>
+          <S.Button onClick={handleClick}>회원 탈퇴하기</S.Button>
         </S.OptionContainer>
       </S.MainContainer>
       <Footer></Footer>
